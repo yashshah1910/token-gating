24e81356b3699879ac23c71c8109128c614025b3
Yash Shah <yashshah192001@gmail.com>
2023-03-01 09:27:58 +0530

    Add MUI Theme

--- src/App.js
+++ src/App.js
@@ -8,6 +8,33 @@ import CssBaseline from "@mui/material/CssBaseline";
 const darkTheme = createTheme({
   palette: {
     mode: "dark",
+
+    // palette values for dark mode
+    primary: {
+      50: "#F0F7FF",
+      100: "#C2E0FF",
+      200: "#99CCF3",
+      300: "#66B2FF",
+      400: "#3399FF",
+      500: "#007FFF",
+      600: "#0072E5",
+      700: "#0059B2",
+      800: "#004C99",
+      900: "#003A75",
+      contrastText: "rgba(0, 0, 0, 0.87)",
+      dark: "#0059B2",
+      light: "#66B2FF",
+      main: "#3399FF",
+    },
+    divider: "rgba(194, 224, 255, 0.08)",
+    background: {
+      default: "#001E3C",
+      paper: "#0A1929",
+    },
+    text: {
+      primary: "#fff",
+      secondary: "#B2BAC2",
+    },
   },
 });
 
